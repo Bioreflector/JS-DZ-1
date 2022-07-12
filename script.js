1b3b212a9cdbd6995cf3afcc365c937ed7402b54
Bioreflector <Bioreflector@gmail.com>
2022-07-12 17:55:31 +0300

    test one

--- script.js
+++ script.js
@@ -47,3 +47,30 @@ function findLongestWordLength(str) {
   }
   
 findLongestWordLength("The quick brown jumped fox over the lazy dog")
+
+// task 5
+function largestOfFour(arr) {
+    const largeNumberArr = []
+    arr.forEach((item) =>{
+        let largeNumber = 0
+        item.forEach((itemElements) =>{
+
+            if(largeNumber < itemElements) largeNumber = itemElements
+            
+        })
+        largeNumberArr.push(largeNumber)
+        
+    })
+    return console.log(largeNumberArr)
+  }
+  
+  largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]])
+
+
+
+  
+  testArr = [ -10 , -1 , -2 , -5]
+  const sortTestArr = testArr.sort((a , b) =>{
+    return a - b
+  })
+  console.log(sortTestArr)
