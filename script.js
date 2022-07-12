db4d7340fdd1b51a6f8634eee084bb07c7369b69
Bioreflector <Bioreflector@gmail.com>
2022-07-12 14:00:16 +0300

    create function Reverse a String

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// task 1
 function convertToF(celsius){
     let farengeit = celsius * 1.8 + 32
     return console.log(`${farengeit} F`)
 }
 convertToF(-22)
+
+// task 2
+
+function reversString(str){
+    return str.split("").reverse().join("");
+}
+console.log(reversString("hellow"))
+
+// function reversString(str){
+//     let newStr = ""
+//     for(let i = str.length -1; i >= 0 ; i --){
+//         newStr += str[i]
+        
+//     }
+//    return newStr
+// }
+//  console.log(reversString('hellow'))
