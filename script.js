0586a8bcddc7be73c6561c776006b18522ecff8d
Bioreflector <Bioreflector@gmail.com>
2022-07-14 12:36:17 +0300

    create function Boo who

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // task 1 Convert Celsius to Fahrenheit
 function convertToF(celsius) {
   let farengeit = celsius * 1.8 + 32;
-  return console.log(`${farengeit} F`);
+  return console.log(`Task 1 result : ${farengeit} F`)
 }
-convertToF(-22);
+convertToF(-22)
 
 // task 2 Reverse a String
 function reversString(str) {
-  return str.split("").reverse().join("");
+  return  console.log(`Task 2 result : ${str.split("").reverse().join("")}`)
 }
-console.log(reversString("hellow"));
+reversString("hellow")
 
 // function reversString(str){
 //     let newStr = ""
@@ -29,10 +29,10 @@ function factorialize(num) {
     for (i = 1; i <= num; i++) {
       result *= i;
     }
-    return result;
+    return console.log(`Task 3 result : ${result}`)
   }
 }
-console.log(factorialize(5));
+factorialize(5);
 
 // task 4 Find the Longest Word in a String
 function findLongestWordLength(str) {
@@ -41,7 +41,7 @@ function findLongestWordLength(str) {
   strArr.forEach((item) => {
     if (strlongest.length < item.length) strlongest = item;
   });
-  return console.log(strlongest.length);
+  return console.log(`Task 4 result : ${strlongest.length}`);
 }
 
 findLongestWordLength("The quick brown jumped fox over the lazy dog");
@@ -56,7 +56,7 @@ function largestOfFour(arr) {
     largeNumberArr.push(largeNumber);
   });
 
-  return console.log(largeNumberArr);
+  return console.log(`Task 5 result : ${largeNumberArr}`);
 }
 largestOfFour([
   [17, 23, 25, 12],
@@ -71,7 +71,7 @@ function confirmEnding(str, target) {
   str.substr(str.length - target.length, target.length) == target
     ? (result = true)
     : (result = false);
-  return console.log(result);
+  return console.log(`Task 6 result : ${result}`);
 }
 
 //   function confirmEnding(str, target) {
@@ -90,16 +90,17 @@ function repeatStringNumTimes(str, num) {
       newStr += str;
     }
   }
-  return console.log(newStr);
+  return console.log(`Task 7 result : ${newStr}`);
 }
 repeatStringNumTimes("*", 8);
+repeatStringNumTimes("abc", 3)
 
 // task 8 Truncate a String
 function truncateString(str, num) {
   if (str.length > num) {
-    return console.log(`${str.slice(0, num)}...`);
+    return console.log(`Task 8 result : ${str.slice(0, num)}...`);
   } else {
-    return console.log(`${str.slice(0, num)}`);
+    return console.log(`Task 8 result : ${str.slice(0, num)}`);
   }
 }
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
@@ -115,7 +116,7 @@ function findElement(arr, func) {
       num = undefined;
     }
   }
-  return console.log(num);
+  return console.log(`Task 9 result : ${num}`);
 }
 findElement([1, 3, 5, 8, 9, 10], function (num) {
   return num % 2 === 0;
@@ -123,4 +124,14 @@ findElement([1, 3, 5, 8, 9, 10], function (num) {
 findElement([1, 3, 5, 9], function (num) {
   return num % 2 === 0;
 });
+
+// task 10 Boo who
+function booWho(bool) {
+    if (typeof bool ==='boolean') bool = true
+    else bool = false
+    return console.log(`Task 10 result : ${bool}`)  
+  }
+  booWho(true)
+  booWho([1, 2, 3])
+
 
