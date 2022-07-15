46f48cd33760e2d1d069f1fc69d5c9b6c6c3f384
Alexandr <bioreflector@gmail.com>
2022-07-15 14:17:16 +0300

    create fucntion Chunky Monkey

--- script.js
+++ script.js
@@ -210,3 +210,19 @@ function booWho(bool) {
   mutation(["ate", "date"])
   mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])
   mutation(["hello", "hey"])
+
+  // task 16 Chunky Monkey
+  function chunkArrayInGroups(arr, size) {
+    const newArr = []
+    for(let i = 0 ; i < arr.length ; i += size){
+        newArr.push(arr.slice(i , i+size))
+    }
+    return  console.log((newArr))
+  }
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4)
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
